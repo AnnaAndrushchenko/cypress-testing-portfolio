14e1a3b86735359ecffc47fea1f25afd305d7e6e
unknown <dedmalgren@gmail.com>
2024-06-12 21:49:14 +0100

    deleted tests

--- cypress/e2e/cypress/e2e/CSSlocators.cy.js
+++
@@ -1,8 +0,0 @@
-describe('Dropdown Click Test', () => {
-    it('Should open dropdown on click', () => {
-        cy.visit('https://rahulshettyacademy.com/dropdownsPractise/')
-        cy.get('#ctl00_mainContent_ddl_originStation1_CTXT').click()
-        cy.get('.paxinfo').click() 
-        cy.get("[name='ctl00$mainContent$view_date1']").click()
-    })
-})
